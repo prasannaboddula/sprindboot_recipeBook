6bb9bf87705f740f19b0c8fd28046b9d55b1a52b
prasannaboddula <prasannaboddula749@gmail.com>
2023-03-08 06:12:36 +0000

    springBoot recipeBook

---
+++ src/main/java/com/example/recipe/RecipeRepository.java
@@ -0,0 +1,11 @@
+package com.example.recipe;
+
+import java.util.*;
+
+public interface RecipeRepository{
+    ArrayList<Recipe> getRecipes();
+    Recipe getRecipeById(int recipeId);
+    Recipe addRecipe(Recipe recipe);
+    Recipe updateRecipe(int recipeId, Recipe recipe);
+    void deleteRecipe(int recipeId);
+}
